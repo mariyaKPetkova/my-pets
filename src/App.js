a862d1445e5c837b7ca1f3ccfadc2ad8a0623414
mariyaKPetkova <90306706+mariyaKPetkova@users.noreply.github.com>
2021-11-23 01:21:39 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import { Routes,Route} from 'react-router-dom'
+
+import Header from './components/Header/Header.js';
+import Create from './components/Create/Create.js'
+import Dashboard from './components/Dashboard/Dashboard.js'
+import Details from './components/Details/Details.js'
+import Edit from './components/Edit/Edit.js'
+import Login from './components/Login/Login.js'
+import MyPets from './components/MyPets/MyPets.js'
+import Register from './components/Register/Register.js'
+function App() {
+  return (
+    <div id="container">
+      <Header />
+      <main id="site-content">
+        <Routes>
+          <Route path="/" element={<Dashboard/>} />
+          <Route path="/details" element={<Details/>} />
+          <Route path="/edit" element={<Edit/>} />
+          <Route path="/login" element={<Login/>} />
+          <Route path="/register" element={<Register/>} />
+          <Route path="/my-pets" element={<MyPets/>} />
+          <Route path="/create" element={<Create/>} />
+        </Routes>
+
+      </main>
+
+      <footer id="site-footer">
+        <p>@PetMyPet</p>
+      </footer>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,23 @@
+import {Link} from 'react-router-dom'
+const Header = () => {
+    return(
+        <header id="site-header">
+            <nav className="navbar">
+                <section className="navbar-dashboard">
+                    <Link to="/">Dashboard</Link>
+                    <div id="guest">
+                        <Link className="button" to="/login">Login</Link>
+                        <Link className="button" to="/register">Register</Link>
+                    </div>
+                    <div id="user">
+                        <span>Welcome, email</span>
+                        <a className="button" href="/my-pets">My Pets</a>
+                        <Link className="button" to="/create">Add Pet</Link>
+                        <Link className="button" to="#">Logout</Link>
+                    </div>
+                </section>
+            </nav>
+        </header>
+    )
+}
+export default Header
